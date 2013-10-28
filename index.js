f88b1a541f77f8b0af39f879e2b571459eae03ea
Emanuel Silva <emanuelbsilva@gmail.com>
2013-10-28 17:02:37 +0000

    Encapsulate middleware

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-module.exports = function(req, res, next) {
-	res.setHeader('Last-Modified', (new Date()).toUTCString());
-	next();
+module.exports = function disable304(){
+	return function(req, res, next) {
+		res.setHeader('Last-Modified', (new Date()).toUTCString());
+		next();
+	}
 }
